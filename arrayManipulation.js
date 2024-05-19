58e62a9d4e258f63646ecbd51072ce412051228e
selijoyy <sjkwawukume@st.ug.edu.gh>
2024-05-19 22:21:21 +0000

    Task 2

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -1,5 +1,5 @@
-function processArray(numArray){
-    return numArray.map (num => {
+function processArray(array){
+    return array.map (num => {
         if(num % 2 === 0){
             return num * num;
         }
@@ -10,5 +10,23 @@ function processArray(numArray){
     );
 }
 
-let selection = processArray([1, 2, 3, 4, 5]);
-console.log(selection);
+let inputArray = [1, 2, 3, 4, 5];
+let resultArray = processArray(inputArray);
+console.log(resultArray);
+
+function formatArrayStrings(stringsArray, numbersArray) {
+    return stringsArray.map((str, index) => {
+        if (numbersArray[index] % 2 === 0) {
+            return str.toUpperCase();
+        }
+        else {
+            return str.toLowerCase();
+        }
+    })
+}
+
+const inputStrings = ['Hello', 'World', 'Goodbye']
+const inputNumbers = [1, 2, 3, 4, 5];
+const processNumbers = processArray(inputNumbers);
+const formattedStrings = formatArrayStrings(inputStrings, processNumbers);
+console.log(formattedStrings);
